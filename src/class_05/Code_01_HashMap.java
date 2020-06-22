9bea98ee90004882d70028f3754c442592e7e4f9
Garen <garen2994@hotmail.com>
2020-06-22 16:10:18 +0800

    :purple_heart: Chap05-1 updated

--- src/class_05/Code_01_HashMap.java
+++ src/class_05/Code_01_HashMap.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
+
 /**
  * @Title :  哈希表与哈希函数的认识
  * @Author : Garen Hou
@@ -12,91 +13,92 @@ import java.util.Map.Entry;
  */
 public class Code_01_HashMap {
 
-	public static void main(String[] args) {
-		HashMap<String, String> map = new HashMap<>();
-		map.put("zuo", "31");
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("garen", "24");
 
-		System.out.println(map.containsKey("zuo"));
-		System.out.println(map.containsKey("chengyun"));
-		System.out.println("=========================");
+        System.out.println(map.containsKey("garen"));
+        System.out.println(map.containsKey("hou"));
+        System.out.println("=========================");
 
-		System.out.println(map.get("zuo"));
-		System.out.println(map.get("chengyun"));
-		System.out.println("=========================");
+        System.out.println(map.get("garen"));
+        System.out.println(map.get("hou"));
+        System.out.println("=========================");
 
-		System.out.println(map.isEmpty());
-		System.out.println(map.size());
-		System.out.println("=========================");
+        System.out.println(map.isEmpty());
+        System.out.println(map.size());
+        System.out.println("=========================");
 
-		System.out.println(map.remove("zuo"));
-		System.out.println(map.containsKey("zuo"));
-		System.out.println(map.get("zuo"));
-		System.out.println(map.isEmpty());
-		System.out.println(map.size());
-		System.out.println("=========================");
+        System.out.println(map.remove("garen"));
+        System.out.println(map.containsKey("garen"));
+        System.out.println(map.get("garen"));
+        System.out.println(map.isEmpty());
+        System.out.println(map.size());
+        System.out.println("=========================");
 
-		map.put("zuo", "31");
-		System.out.println(map.get("zuo"));
-		map.put("zuo", "32");
-		System.out.println(map.get("zuo"));
-		System.out.println("=========================");
+        map.put("garen", "24");
+        System.out.println(map.get("garen"));
+        map.put("garen", "18");
+        System.out.println(map.get("garen"));
+        System.out.println("=========================");
 
-		map.put("zuo", "31");
-		map.put("cheng", "32");
-		map.put("yun", "33");
+        map.put("garen", "24");
+        map.put("hou", "18");
+        map.put("tong", "22");
 
-		for (String key : map.keySet()) {
-			System.out.println(key);
-		}
-		System.out.println("=========================");
+        for (String key : map.keySet()) {
+            System.out.println(key);
+        }
+        System.out.println("=========================");
 
-		for (String values : map.values()) {
-			System.out.println(values);
-		}
-		System.out.println("=========================");
+        for (String values : map.values()) {
+            System.out.println(values);
+        }
+        System.out.println("=========================");
 
-		map.clear();
-		map.put("A", "1");
-		map.put("B", "2");
-		map.put("C", "3");
-		map.put("D", "1");
-		map.put("E", "2");
-		map.put("F", "3");
-		map.put("G", "1");
-		map.put("H", "2");
-		map.put("I", "3");
-		for (Entry<String, String> entry : map.entrySet()) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-			System.out.println(key + "," + value);
-		}
-		System.out.println("=========================");
+        map.clear();
+        map.put("A", "1");
+        map.put("B", "2");
+        map.put("C", "3");
+        map.put("D", "1");
+        map.put("E", "2");
+        map.put("F", "3");
+        map.put("G", "1");
+        map.put("H", "2");
+        map.put("I", "3");
+        for (Entry<String, String> entry : map.entrySet()) {  //entrySet+foreach遍历HashMap (JDK1.8特性)
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + "," + value);
+        }
+        System.out.println("=========================");
 
-		// you can not remove item in map when you use the iterator of map
-//		 for(Entry<String,String> entry : map.entrySet()){
-//			 if(!entry.getValue().equals("1")){
-//				 map.remove(entry.getKey());
-//			 }
-//		 }
+//		 you can not remove item in map when you use the iterator of map
+//	    try {
+//		    for (Entry<String, String> entry : map.entrySet()) {
+//		        if (!entry.getValue().equals("1")) {
+//		            map.remove(entry.getKey());
+//		        }
+//		    }
+//	    } catch (Exception e) {
+//	        throw new RuntimeException("Wrong Operation");
+//	    }
 
-		// if you want to remove items, collect them first, then remove them by
-		// this way.
-		List<String> removeKeys = new ArrayList<String>();
-		for (Entry<String, String> entry : map.entrySet()) {
-			if (!entry.getValue().equals("1")) {
-				removeKeys.add(entry.getKey());
-			}
-		}
-		for (String removeKey : removeKeys) {
-			map.remove(removeKey);
-		}
-		for (Entry<String, String> entry : map.entrySet()) {
-			String key = entry.getKey();
-			String value = entry.getValue();
-			System.out.println(key + "," + value);
-		}
-		System.out.println("=========================");
 
-	}
-
+        // if you want to remove items, collect them first, then remove them by this way.
+        List<String> removeKeys = new ArrayList<>();
+        for (Entry<String, String> entry : map.entrySet()) {
+            if (!entry.getValue().equals("1")) {
+                removeKeys.add(entry.getKey());
+            }
+        }
+        for (String key : removeKeys) {
+            map.remove(key);
+        }
+	    for (Entry<String, String> entry : map.entrySet()) {
+		    String key = entry.getKey();
+		    String value = entry.getValue();
+		    System.out.println(key + "," + value);
+	    }
+    }
 }

716102d492ebdeb090d0e702b6ee2ba999a018de
Garen <garen2994@hotmail.com>
2020-06-24 18:24:58 +0800

    :purple_heart: Chap05-2 updated

--- src/class_05/Code_02_RandomPool.java
+++ src/class_05/Code_02_RandomPool.java
@@ -1,67 +1,72 @@
 package class_05;
 
 import java.util.HashMap;
+import java.util.Random;
+
 /**
  * @Title : 设置RandomPool结构
- * @Description : 实现三个功能(两个HashMap)--操作达到常量时间---不允许重复--等概率随机访问
+ * @Description : 实现三个功能--操作达到常量时间---不允许重复--等概率随机访问
+ * 用两个HashMap或动态数组+HashMap (LeetCode 380)
  * @Author : Garen Hou
  * @Email : garen2994@hotmail.com
  * @Date : 2020/6/22 16:31
  */
 public class Code_02_RandomPool {
 
-	public static class Pool<K> {
-		private HashMap<K, Integer> keyIndexMap;
-		private HashMap<Integer, K> indexKeyMap;
-		private int size;
+    public static class Pool<K> {
+        private HashMap<K, Integer> keyIndexMap;
+        private HashMap<Integer, K> indexKeyMap;
+        private int size;
 
-		public Pool() {
-			this.keyIndexMap = new HashMap<K, Integer>();
-			this.indexKeyMap = new HashMap<Integer, K>();
-			this.size = 0;
-		}
+        public Pool() {
+            this.keyIndexMap = new HashMap<>();
+            this.indexKeyMap = new HashMap<>();
+            this.size = 0;
+        }
 
-		public void insert(K key) {
-			if (!this.keyIndexMap.containsKey(key)) {
-				this.keyIndexMap.put(key, this.size);
-				this.indexKeyMap.put(this.size++, key);
-			}
-		}
+        public void insert(K key) {
+            if (!this.keyIndexMap.containsKey(key)) {
+                this.keyIndexMap.put(key, this.size);
+                this.indexKeyMap.put(this.size++, key);
+            }
+        }
 
-		public void delete(K key) {
-			if (this.keyIndexMap.containsKey(key)) {
-				int deleteIndex = this.keyIndexMap.get(key);
-				int lastIndex = --this.size;
-				K lastKey = this.indexKeyMap.get(lastIndex);
-				this.keyIndexMap.put(lastKey, deleteIndex);
-				this.indexKeyMap.put(deleteIndex, lastKey);
-				this.keyIndexMap.remove(key);
-				this.indexKeyMap.remove(lastIndex);
-			}
-		}
+        public void delete(K key) {
+            if (this.keyIndexMap.containsKey(key)) {
+                int deleteIndex = this.keyIndexMap.get(key);
+                int lastIndex = --this.size;
+                K lastKey = this.indexKeyMap.get(lastIndex);
+                this.keyIndexMap.put(lastKey, deleteIndex);
+                this.indexKeyMap.put(deleteIndex, lastKey);
+                this.keyIndexMap.remove(key);
+                this.indexKeyMap.remove(lastIndex);
+            }
+        }
 
-		public K getRandom() {
-			if (this.size == 0) {
+        public K getRandom() {
+            if (this.size == 0) {
                 return null;
             }
-            int randomIndex = (int) (Math.random() * this.size); // 0 ~ size -1
-            return this.indexKeyMap.get(randomIndex);
-		}
+            Random r = new Random();
+            int idx = r.nextInt(this.size);
+            return this.indexKeyMap.get(idx);
+        }
+    }
 
-	}
+    public static void main(String[] args) {
+        Pool<String> pool = new Pool<String>();
+        pool.insert("1");
+        pool.insert("2");
+        pool.insert("3");
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+        pool.delete("1");
+        pool.delete("2");
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
+        System.out.println(pool.getRandom());
 
-	public static void main(String[] args) {
-	Pool<String> pool = new Pool<String>();
-		pool.insert("zuo");
-		pool.insert("cheng");
-		pool.insert("yun");
-		System.out.println(pool.getRandom());
-		System.out.println(pool.getRandom());
-		System.out.println(pool.getRandom());
-		System.out.println(pool.getRandom());
-		System.out.println(pool.getRandom());
-		System.out.println(pool.getRandom());
-
-}
+    }
 
 }

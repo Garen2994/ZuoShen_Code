93191f0c8058cdfda251c4c8b2fc7504c56945fc
Garen <garen2994@hotmail.com>
2020-06-24 23:08:40 +0800

    :purple_heart: Union-Find Set updated

--- src/class_05/Code_04_UnionFind.java
+++ src/class_05/Code_04_UnionFind.java
@@ -1,75 +1,81 @@
 package class_05;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
 /**
  * @Title : 设计并查集
  * @Description : 检查两个元素是否属于同一集合
- *                元素A，B所在集合合并为一个集合
+ * 元素A，B所在集合合并为一个集合
  * @Author : Garen Hou
  * @Email : garen2994@hotmail.com
  * @Date : 2020/6/24 20:53
  */
 public class Code_04_UnionFind {
 
-	public static class Node {
-		// whatever you like
-	}
+    public class Node {
+        char value;
 
-	public static class UnionFindSet {
-		public HashMap<Node, Node> fatherMap;
-		public HashMap<Node, Integer> sizeMap;
+	    public Node(char value) {
+		    this.value = value;
+	    }
+    }
 
-		public UnionFindSet() {
-			fatherMap = new HashMap<Node, Node>();
-			sizeMap = new HashMap<Node, Integer>();
-		}
+    public class UnionFindSet {
+        public HashMap<Node, Node> fatherMap;
+        public HashMap<Node, Integer> sizeMap;
 
-		public void makeSets(List<Node> nodes) {
-			fatherMap.clear();
-			sizeMap.clear();
-			for (Node node : nodes) {
-				fatherMap.put(node, node);
-				sizeMap.put(node, 1);
-			}
-		}
+        public UnionFindSet() {
+            fatherMap = new HashMap<Node, Node>();
+            sizeMap = new HashMap<Node, Integer>();
+        }
+		/**
+		 * @description 构建并查集
+		 * @param nodes
+		 * @return void
+		 */
+        public void makeSets(List<Node> nodes) {
+            fatherMap.clear();
+            sizeMap.clear();
+            for (Node node : nodes) {
+                fatherMap.put(node, node);
+                sizeMap.put(node, 1);
+            }
+        }
 
-		private Node findHead(Node node) {
-			Node father = fatherMap.get(node);
-			if (father != node) {
-				father = findHead(father);
-			}
-			fatherMap.put(node, father);
-			return father;
-		}
-		
-		public boolean isSameSet(Node a, Node b) {
-			return findHead(a) == findHead(b);
-		}
+        private Node findHead(Node node) {
+            Node father = fatherMap.get(node);
+            //recurse until father = node
+            if (father != node) {
+                father = findHead(father);
+            }
+            fatherMap.put(node, father);
+            return father;
+        }
 
-		public void union(Node a, Node b) {
-			if (a == null || b == null) {
-				return;
-			}
-			Node aHead = findHead(a);
-			Node bHead = findHead(b);
-			if (bHead != aHead) {
-				int aSetSize= sizeMap.get(aHead);
-				int bSetSize = sizeMap.get(bHead);
-     				if (aSetSize <= bSetSize) {
-					fatherMap.put(aHead, bHead);
-					sizeMap.put(bHead, aSetSize + bSetSize);
-				} else {
-					fatherMap.put(bHead, aHead);
-					sizeMap.put(aHead, aSetSize + bSetSize);
-				}
-			}
-		}
+        public boolean isSameSet(Node a, Node b) {
+            return findHead(a) == findHead(b);
+        }
 
-	}
-
-	public static void main(String[] args) {
-
-	}
+        public void union(Node a, Node b) {
+            if (a == null || b == null) {
+                return;
+            }
+            Node aHead = findHead(a);
+            Node bHead = findHead(b);
+            if (bHead != aHead) {
+                int aSetSize = sizeMap.get(aHead);
+                int bSetSize = sizeMap.get(bHead);
+                if (aSetSize <= bSetSize) {
+                    fatherMap.put(aHead, bHead);
+                    sizeMap.put(bHead, aSetSize + bSetSize);
+                } else {
+                    fatherMap.put(bHead, aHead);
+                    sizeMap.put(aHead, aSetSize + bSetSize);
+                }
+            }
+        }
+    }
 
 }

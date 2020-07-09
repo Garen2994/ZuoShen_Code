8b51af1f7c6b1f927a4a19343fae5211f2f08b1c
garen <garen2994@hotmail.com>
2020-07-09 21:20:16 +0800

    :purple_heart: Practice updated

--- src/class_01/QuickSort_1.java
+++ src/class_01/QuickSort_1.java
@@ -7,59 +7,78 @@ package class_01;
  */
 public class QuickSort_1 {
     public static void quickSort_1(int[] arr){
-        if(arr == null || arr.length <= 2) {
-            return;
+        if(arr == null || arr.length < 2){
+            throw new RuntimeException("Wrong array");
         }
-        quickSort(arr,0,arr.length - 1);
+        quickSort(arr, 0 ,arr.length - 1);
     }
-    public static void quickSort(int[] arr,int l,int r){
+    public static void quickSort(int[] arr, int l , int r){
         if(l < r){
 //            int p = partition(arr, l, r);
-            int p = partitionTwoWay(arr, l, r);
-            quickSort(arr,l,p - 1);
-            quickSort(arr,p + 1, r);
+//            int p = partition2Way(arr, l, r);
+            int[] p = partition3Way(arr, l, r);
+            quickSort(arr,l,p[0]);
+            quickSort(arr,p[1], r);
+            
+//            quickSort(arr,l,p - 1);
+//            quickSort(arr,p + 1, r);
         }
     }
-    public static int partition(int[] arr, int l ,int r){
-        swap(arr,l,l +(int)(Math.random()*(r - l + 1)));
+    private static int partition(int[] arr,int l, int r){
+        swap(arr,l,l+(int)(Math.random()*(r - l + 1)));
+        int key = arr[l];
         int j = l;
-        int v = arr[l];
-        for(int i = j + 1; i <= r; i++){
-            if(arr[i] < v){
-                j++;
-                swap(arr,j,i);
+        for (int i = j + 1; i <= r; i++) {
+            if(arr[i] < key){
+                swap(arr, i, ++j);
             }
         }
-        swap(arr,l,j);
+        swap(arr, j, l);
         return j;
     }
-    public static int partitionTwoWay(int[] arr, int l, int r){
-        swap(arr,l,l + (int) Math.random() * (r-l+1));
-        int v = arr[l];
+    public static int partition2Way(int[] arr,int l, int r){
+        swap(arr,l,l+(int)(Math.random() * (r - l + 1)));
+        int key = arr[l];
         while(l < r){
-            while(l < r && arr[l] < v){
-                l++;
-            }
-            arr[r] = arr[l];
-            while(l < r && arr[r] > v){
+            while(l < r && arr[r] >= key){
                 r--;
             }
             arr[l] = arr[r];
+            while(l < r && arr[l] <= key){
+                l++;
+            }
+            arr[r] = arr[l];
         }
-        arr[l] = v;
+        //此时l和r相遇
+        arr[l] = key;
         return l;
     }
-    private static void swap(int[] arr, int i, int j){
-//        arr[i] = arr[i] ^ arr[j];
-//        arr[j] = arr[i] ^ arr[j];
-//        arr[i] = arr[i] ^ arr[j];
+    public static int[] partition3Way(int[] arr,int l, int r){
+        swap(arr,l,l + (int)(Math.random() * (r - l + 1)));
+        int key = arr[l];
+        int lt = l;  //arr[l+1 , lt] < key
+        int gt = r + 1; // arr[gt + 1,r] > key
+        int i = l + 1; // arr[lt+1, i] = key
+        while(i < gt){
+            if(arr[i] < key){
+                swap(arr,i++,++lt);
+            }else if(arr[i] > key){
+                swap(arr, i, --gt);
+            }else{
+                i++;
+            }
+        }
+        swap(arr,l,lt);
+        return new int[]{lt, gt};
+    }
+    private static void swap(int[] arr, int i ,int j){
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
-    }
     
+    }
     public static void main(String[] args) {
-        int[] arr = {4,6,2,3,1,10,7,8};
+        int[] arr = {4,6,4,5,6,-2,1,7,-2,3,1,10,7,8};
         quickSort_1(arr);
         for (int i : arr) {
             System.out.print(i+" ");
--- src/class_01/quickSort_Test.java
+++ src/class_01/quickSort_Test.java
@@ -8,13 +8,13 @@ public class quickSort_Test {
     @Test
     public void Test(){
         QuickSort_practice1 sort1 = new QuickSort_practice1();
-        int[] arr = {4,8,12,5,9,32,-5};
+        int[] arr = {11,8,12,5,9,32,6};
         sort1.quickSort(arr);
         System.out.println(Arrays.toString(arr));
     }
     @Test
     public void Test_2(){
-        int[] arr = {3,-4,5,-5,1,8,10,6};
+        int[] arr = {3,8,6,-5,5,-4,5,-5,1,3,-1,6,-5,8,10,6};
         QuickSort qs = new QuickSort();
         qs.quickSort(arr);
         System.out.println(Arrays.toString(arr));

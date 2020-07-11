34b59a07c2ec4eaec8728e8036609efdbc96b93b
Garen <garen2994@hotmail.com>
2020-07-11 18:11:35 +0800

    :purple_heart: Practice updated

--- src/class_01/QuickSort_1.java
+++ src/class_01/QuickSort_1.java
@@ -14,30 +14,28 @@ public class QuickSort_1 {
     }
     public static void quickSort(int[] arr, int l , int r){
         if(l < r){
-//            int p = partition(arr, l, r);
-//            int p = partition2Way(arr, l, r);
-            int[] p = partition3Way(arr, l, r);
-            quickSort(arr,l,p[0]);
-            quickSort(arr,p[1], r);
-            
-//            quickSort(arr,l,p - 1);
-//            quickSort(arr,p + 1, r);
+//            int p = partition2Way(arr, l ,r);
+//            quickSort(arr, l, p - 1);
+//            quickSort(arr, p + 1, r);
+            int[] p = partition3Way(arr,l,r);
+            quickSort(arr, l, p[0] - 1);
+            quickSort(arr, p[0] + 1, r);
         }
     }
     private static int partition(int[] arr,int l, int r){
-        swap(arr,l,l+(int)(Math.random()*(r - l + 1)));
+        swap(arr, l, l + (int)(Math.random() * (r - l + 1)));
         int key = arr[l];
         int j = l;
-        for (int i = j + 1; i <= r; i++) {
+        for (int i = j + 1; i <= r; i++){
             if(arr[i] < key){
-                swap(arr, i, ++j);
+                swap(arr, ++j ,i);
             }
         }
-        swap(arr, j, l);
+        swap(arr, l ,j);
         return j;
     }
     public static int partition2Way(int[] arr,int l, int r){
-        swap(arr,l,l+(int)(Math.random() * (r - l + 1)));
+        swap(arr, l, l + (int)(Math.random() * (r - l + 1)));
         int key = arr[l];
         while(l < r){
             while(l < r && arr[r] >= key){
@@ -49,27 +47,24 @@ public class QuickSort_1 {
             }
             arr[r] = arr[l];
         }
-        //此时l和r相遇
         arr[l] = key;
         return l;
     }
     public static int[] partition3Way(int[] arr,int l, int r){
-        swap(arr,l,l + (int)(Math.random() * (r - l + 1)));
-        int key = arr[l];
-        int lt = l;  //arr[l+1 , lt] < key
-        int gt = r + 1; // arr[gt + 1,r] > key
-        int i = l + 1; // arr[lt+1, i] = key
-        while(i < gt){
-            if(arr[i] < key){
-                swap(arr,i++,++lt);
-            }else if(arr[i] > key){
-                swap(arr, i, --gt);
-            }else{
-                i++;
-            }
+        swap(arr, r, l + (int)(Math.random() * (r - l + 1)));
+        int less = l - 1;
+        int more = r;
+        while(l < more){
+           if(arr[l] < arr[r]){
+               swap(arr, ++less, l++);
+           } else if(arr[l] > arr[r]){
+               swap(arr, --more, l);
+           }else{
+               l++;
+           }
         }
-        swap(arr,l,lt);
-        return new int[]{lt, gt};
+        swap(arr, more, r);
+        return new int[]{less + 1,more};
     }
     private static void swap(int[] arr, int i ,int j){
         int tmp = arr[i];
